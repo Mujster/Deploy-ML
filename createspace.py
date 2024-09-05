e2ced06ca4c3034c9642da8c591c593fb63bb148
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-09-06 00:22:04 +0500

    Final Commit

---
+++ createspace.py
@@ -0,0 +1,9 @@
+from huggingface_hub import HfApi
+import os
+
+def create_space():
+    api = HfApi()
+    api.create_repo(name='my-space', repo_type='space', token=os.getenv('HF_TOKEN'))
+
+if __name__ == "__main__":
+    create_space()
