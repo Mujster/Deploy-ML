2e48f309a9ce8689bccbacbcf0afdff457642311
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-09-06 22:26:16 +0500

    Final Commit

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 # app.py
 from flask import Flask, request, jsonify,render_template
-import pickle
 import numpy as np
+import joblib 
 
 app = Flask(__name__)
 
-# Load the trained model
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# Load the pre-trained model
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-
-    # Convert data into numpy array for prediction
-    features = np.array(data['features']).reshape(1, -1)
     
+    # Convert features to a numpy array
+    features = np.array(data['features']).reshape(1, -1)
+
     # Make prediction
     prediction = model.predict(features)
-    
-    # Return prediction result
+
+    # Return prediction
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
